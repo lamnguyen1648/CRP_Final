d8b1b1a13fc9a660fcc740db14e559449b23f479
Lam Nguyen <77882073+lamnguyen1648@users.noreply.github.com>
2021-12-31 14:22:18 +0700

    Delete js directory

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-let arrow = document.querySelectorAll(".arrow");
-console.log(arrow);
-for(var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (a)=>{
-        let arrowParent = a.target.parentElement.parentElement;
-        console.log(arrowParent);
-        arrowParent.classList.toggle("showMenu");
-    });
-}
